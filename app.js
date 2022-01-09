10b0b40dbc3d6152f2158aedeafc740f40cc547c
althafdaa <92288572+althafdaa@users.noreply.github.com>
2022-01-09 18:28:53 +0700

    game is playable in console, added classlist add for selected choice

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
+// Abstraction
 class Player {
   constructor() {
-    this.batu = document.querySelector('.batu');
-    this.gunting = document.querySelector('.gunting');
-    this.kertas = document.querySelector('.kertas');
+    // DOM Selector
+    this.pilihan = document.querySelectorAll('li img');
+    this.reset = document.querySelector('.reset');
+  }
+
+  // method
+  gameStart() {
+    this.pilihan.forEach((selections) => {
+      selections.addEventListener('click', (selected) => {
+        // function
+        const pilihanKomputer = () => {
+          const random = Math.floor(Math.random() * 3 + 1);
+          if (random === 1) return 'batu';
+          if (random === 2) return 'gunting';
+          if (random === 3) return 'kertas';
+        };
+
+        const pilihanPlayer = selected.target.className;
+
+        if (pilihanPlayer == pilihanKomputer()) return console.log('SERI');
+        if (pilihanPlayer == 'batu')
+          return pilihanKomputer() == 'gunting'
+            ? console.log('WIN')
+            : console.log('LOSE');
+        if (pilihanPlayer == 'gunting')
+          return pilihanKomputer() == 'kertas'
+            ? console.log('WIN')
+            : console.log('LOSE');
+        if (pilihanPlayer == 'kertas')
+          return pilihanKomputer() == 'batu'
+            ? console.log('WIN')
+            : console.log('LOSE');
+      });
+    });
   }
 }
 
-// random number from computer
-const pilihanKomputer = () => {
-  const random = Math.floor(Math.random() * 3 + 1);
-  if (random === 1) return console.log('batu');
-  if (random === 2) return console.log('gunting');
-  if (random === 3) return console.log('kertas');
-};
+class NewPlayer extends Player {
+  constructor() {
+    super();
+  }
 
-pilihanKomputer();
+  selectChoice() {
+    this.pilihan.forEach((selected) => {
+      selected.addEventListener('click', (selected) => {
+        document
+          .querySelector(`.${selected.target.className}`)
+          .classList.add('player-select');
+      });
+    });
+  }
+}
+
+// newObject
+const newGame = new Player();
+const SelectPlayer = new NewPlayer();
+
+console.log(newGame.gameStart());
+
+console.log(SelectPlayer.selectChoice());
